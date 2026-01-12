008577ee40e3401ce7bf1a00067e488bc0bfc5cc
istutix123-lang <istutix123@gmail.com>
2026-01-12 16:42:18 +0530

    Kilometer to miles

---
+++ basic-programms/ConvertKilometersToMiles.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ConvertKilometersToMiles{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.print("kilometer=");
+	double Kilometer =sc.nextDouble();
+	double a=0.621371;
+	double Miles = Kilometer*a;
+	System.out.println("Miles  = " +Miles);
+	}
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
