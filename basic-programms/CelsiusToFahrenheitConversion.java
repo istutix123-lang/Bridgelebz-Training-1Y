cd86308e15470207a9643e9dcb958271ceaa9641
istutix123-lang <istutix123@gmail.com>
2026-01-12 16:34:41 +0530

    Convert celcius to fahrenheit

---
+++ basic-programms/CelsiusToFahrenheitConversion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class CelsiusToFahrenheitConversion{
+	public static void main(String[] args){
+	Scanner sc= new Scanner(System.in);
+	System.out.println("enter the temp.in celcius");
+	float celcius=sc.nextFloat();
+	float fahrenheit=(celcius*9/5)+32;
+	System.out.println("fahrenheit="+fahrenheit);
+	}
+	}
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
