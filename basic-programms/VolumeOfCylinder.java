fc897c5c558ecf388bb039e2ce9f81f81aff7fb7
istutix123-lang <istutix123@gmail.com>
2026-01-12 16:35:30 +0530

    Find volume of cylinder

---
+++ basic-programms/VolumeOfCylinder.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class VolumeOfCylinder{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.print("radius=");
+	double radius =sc.nextDouble();
+	System.out.print("height=");
+	double height = sc.nextDouble();
+	double pi=3.14;
+	double volume=pi*radius*radius*height;
+	System.out.println("volume of cylinder  = "+volume);
+	}
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
