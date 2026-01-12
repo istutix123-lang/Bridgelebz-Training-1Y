20982209f8904f9792f862c98c473f5cfb695896
istutix123-lang <istutix123@gmail.com>
2026-01-12 16:40:26 +0530

    Average of three no.

---
+++ basic-programms/CalculateAverageOfThreeNumbers.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class  CalculateAverageOfThreeNumbers{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.print("a =");
+	int a =sc.nextInt();
+	System.out.print("b =");
+	int b =sc.nextInt();
+	System.out.print("c =");
+	int c =sc.nextInt();
+	double Average=(a+b+c)/3;
+	System.out.println("Average= " +Average);
+	}
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
