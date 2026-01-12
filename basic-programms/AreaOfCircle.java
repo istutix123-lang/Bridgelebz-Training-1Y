d60ac380f8788c126175ad4b230b5e1b491243dd
istuti123-lang <istutix123@gmail.com>
2026-01-12 13:21:13 +0530

    Area of cicle

---
+++ basic-programms/AreaOfCircle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("radius=");
+	double radius =sc.nextDouble();
+	  double  pi=3.14;
+	double  area=pi*radius*radius;
+	System.out.println("area of circle ="+area);
+	}
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
