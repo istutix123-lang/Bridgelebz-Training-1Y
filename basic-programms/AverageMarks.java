0ab293ca5d4e092e3f207897bf1f4c85bca36bca
istutix123-lang <istutix123@gmail.com>
2026-01-15 23:41:54 +0530

    [Istuti]Add calculate the averge marks in pcm

---
+++ basic-programms/AverageMarks.java
@@ -0,0 +1,9 @@
+public class AverageMarks{
+	public static void main(String[] args){
+    int maths=94;
+		int physics=95;
+		int chemistry=96;
+		int avg=(94+95+96)/3;
+		System.out.println("Sam's averge marks in PCM is " + avg);
+		}
+		}
