3c2f7fab539b2e04a3dadfce37e0b022033a30f2
istutix123-lang <istutix123@gmail.com>
2026-01-15 23:56:06 +0530

    [Istuti]Add calculate profit loss

---
+++ basic-programms/CalculateProfitLoss.java
@@ -0,0 +1,10 @@
+public class CalculateProfitLoss{
+public static void main(String[] args ){
+double CP=129;
+double SP=191;
+double profit=SP-CP;
+double profitpercentege=(profit/CP)*100;
+System.out.println("The cost price in INR = " + CP + " and The selling price in INR = "+ SP+ " \nThe profit is INR = " + profit + " and the profit percentage = " + profitpercentege);
+}
+}
+  
