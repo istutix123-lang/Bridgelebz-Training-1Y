6838266ef03cf7fddc1698eb19acb7f03eee45f8
istuti123-lang <istutix123@gmail.com>
2026-01-22 22:01:32 +0530

    [Istuti]Add.CountdownForRocketlaunch

---
+++ basic-programms/CountdownForRocketlaunch.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CountdownForRocketlaunch{
+public static void main(String[] args){
+	Scanner sc = new Scanner (System.in);
+	
+			System.out.print(" print no:- ");
+int i=sc.nextInt();
+	
+	while(i>=1){
+		System.out.println(i);
+		i--;
+	}
+			System.out.println("Lanch!");
+
+	
+}
+}
+
+	
+
