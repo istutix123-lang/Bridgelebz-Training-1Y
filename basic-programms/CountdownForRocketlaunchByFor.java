70e52e2f7a84759d281b1076dd7cdb70ca3ed17d
istuti123-lang <istutix123@gmail.com>
2026-01-26 20:43:21 +0530

    [Istuti]Add.CountdownForRocketlaunchByForloop

---
+++ basic-programms/CountdownForRocketlaunchByFor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CountdownForRocketlaunchByFor{
+public static void main(String[] args){
+	Scanner sc = new Scanner (System.in);
+	
+			System.out.print(" print no:- ");
+
+	for(int i=sc.nextInt();i>=1;i--){
+			System.out.println(i);
+}
+			System.out.println("Lanch!");
+
+	
+}
+}
+
+
