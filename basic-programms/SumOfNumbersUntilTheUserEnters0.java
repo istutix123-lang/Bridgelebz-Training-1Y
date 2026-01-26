209ce8284c2c75e3c2d46568bf1396d8b05124b2
istuti123-lang <istutix123@gmail.com>
2026-01-26 20:47:55 +0530

    [Istuti]Add.SumOfNumbersUntilTheUserEnters0

---
+++ basic-programms/SumOfNumbersUntilTheUserEnters0.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class SumOfNumbersUntilTheUserEnters0{
+public static void main(String[] args){
+	Scanner sc = new Scanner (System.in);
+	
+			System.out.print("enter a nunber :- ");
+			if (int a==0){
+							System.out.print("00 :- ");
+
+				
+				
+				
+				
+				
+				
+} else {
+				System.out.print("enter a input again :- ");
+
+	
+
+	
+}
+}
+
+	
+
